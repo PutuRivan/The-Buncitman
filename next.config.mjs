d15f3646af6aef21a4504cbc122863bf46b76078
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2024-11-14 21:22:27 +0700

    feat(databases) : menambahkan prisma

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
         remotePatterns:[
             {
                 hostname:'placehold.co'
+            },
+            {
+                hostname:'res.cloudinary.com'
             }
         ]
     }
