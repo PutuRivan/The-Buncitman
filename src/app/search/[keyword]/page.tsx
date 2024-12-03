a9a204e0ca116557661e5aab3ffa8a9d154e5625
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2024-12-03 17:07:24 +0700

    fix:error paramss

--- src/app/search/[keyword]/page.tsx
+++ src/app/search/[keyword]/page.tsx
@@ -3,44 +3,52 @@ import Product from "@/components/Card/Product";
 import { getSearchQuery } from "@/lib/action/product";
 import React from "react";
 
-interface ProductType {
-  id: string;
-  name: string;
-  price: number; // Ganti dengan tipe yang sesuai
-  imageUrl: string;
-}
-
 interface Props {
   params: {
     keyword: string;
   };
 }
-const page = async ({ params }: Props) => {
-  const { keyword } = params;
-  const decodedKeyword = decodeURIComponent(keyword);
 
-  const product: ProductType[] = await getSearchQuery(decodedKeyword);
-  return (
-    <>
-      <Header />
-      <div className="p-5 flex flex-col gap-10">
-        <h2 className="text-Heading-2 font-bold">
-          Hasil Pencarian Untuk {decodedKeyword}
-        </h2>
-        <div className="grid grid-cols-4 gap-10 mx-10">
-          {product.map((item) => (
-            <Product
-              key={item.id}
-              id={item.id}
-              name={item.name}
-              price={item.price}
-              imageUrl={item.imageUrl}
-            />
-          ))}
-        </div>
-      </div>
-    </>
-  );
+interface ProductType {  
+  id: string;  
+  name: string;  
+  price: number;  
+  imageUrl: string;  
+}  
+
+const Page = async ({ params }: Props) => {  
+  const { keyword } = params;  
+  const decodedKeyword = decodeURIComponent(keyword);  
+  
+  let products: ProductType[] = [];  
+
+  try {  
+    products = await getSearchQuery(decodedKeyword);  
+  } catch (error) {  
+    console.error('Error fetching products:', error);  
+    // Anda dapat mengatur state untuk menampilkan pesan kesalahan  
+  }  
+
+  return (  
+    <>  
+      <Header />  
+      <div className="p-5 flex flex-col gap-10">  
+        <h2 className="text-Heading-2 font-bold">  
+          Hasil Pencarian Untuk: {decodedKeyword}  
+        </h2>  
+        <div className="grid grid-cols-4 gap-10 mx-10">  
+          {products.map((item) => (  
+            <Product  
+              key={item.id}  
+              id={item.id}  
+              name={item.name}  
+              price={item.price}  
+              imageUrl={item.imageUrl}  
+            />  
+          ))}  
+        </div>  
+      </div>  
+    </>  
+  );  
 };
-
-export default page;
+export default Page;
